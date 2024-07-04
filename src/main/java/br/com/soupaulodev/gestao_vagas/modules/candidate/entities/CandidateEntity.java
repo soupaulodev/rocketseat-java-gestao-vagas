828ab17533c9f89a9d4b069cb71697b3cf568b4e
Paulo Marques <paulomarques.rs1@gmail.com>
2024-07-04 01:26:39 -0300

    feat: Criando representacao com da tabela Candidate

--- src/main/java/br/com/soupaulodev/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/br/com/soupaulodev/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,19 +1,31 @@
 package br.com.soupaulodev.gestao_vagas.modules.candidate.entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+    
+    @Id
+    @Generated(value = "UUID")
     private UUID id;
+    
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não pode conter espaços")
+    @NotBlank
+    @Pattern(regexp = "^\\S+$", message = "O campo [username] não pode conter espaços")
     private String username;
 
     @Email(message = "O campo [email] deve conter um email válido")
@@ -23,4 +35,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
